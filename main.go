99f28f0089bae8628976ca90cba58f79a882990a
e-berman <ethanjamesberman@gmail.com>
2023-03-28 13:00:53 -0700

    refactor w db pool

--- main.go
+++ main.go
@@ -1,49 +1,22 @@
 package main
 
 import (
-	"context"
 	"log"
-	"net/http"
-	"os"
-	"os/signal"
-	"time"
-	"github.com/e-berman/baseball_api/handlers"
 )
 
 func main() {
-	l := log.New(os.Stdout, "baseball-api ", log.LstdFlags)
-	ph := handlers.NewPlayer(l)
+	dbpool, err := NewDBPool()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer dbpool.db.Close()
 
-	sm := http.NewServeMux()
-	sm.Handle("/", ph)
-
-	s := &http.Server{
-		Addr: ":4242",
-		Handler: sm,
-		IdleTimeout: 120 * time.Second,
-		ReadTimeout: 1 * time.Second,
-		WriteTimeout: 1 * time.Second,
+	if err := dbpool.InitializePlayerTable(); err != nil {
+		log.Fatal(err)
+	} else {
+		
 	}
 
-	go func() {
-		l.Println("Starting server on port 4242")
-
-		err := s.ListenAndServe()
-		if err != nil {
-			l.Printf("Error starting server: %s\n", err)
-			os.Exit(1)
-		}
-	}()
-	
-	sigChan := make(chan os.Signal)
-	signal.Notify(sigChan, os.Interrupt)
-	signal.Notify(sigChan, os.Kill)
-	
-	sig := <- sigChan
-	l.Println("Server has been gracefully shutdown.", sig)
-	
-	tc, cancel := context.WithTimeout(context.Background(), 30 * time.Second)
-	defer cancel()
-
-	s.Shutdown(tc)
+	server := NewServer(":4242", dbpool)
+	server.StartServer()
 }
