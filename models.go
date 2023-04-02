093e00dee85c0df66b16a4154c01d5c1de82abc2
e-berman <ethanjamesberman@gmail.com>
2023-04-01 22:40:17 -0700

    remove field and teams struct

--- models.go
+++ models.go
@@ -3,9 +3,8 @@ package main
 import "time"
 
 type CreatePlayerRequest struct {
-	PlayerName		string	`json:"playerName"`
+	Name			string	`json:"name"`
 	Team			string  `json:"team"`
-	Position		string	`json:"position"`
 	Games			int	`json:"games"`
 	PA			int	`json:"plateAppearances"`
 	HR			int	`json:"homeRuns"`
@@ -28,9 +27,8 @@ type CreatePlayerRequest struct {
 }
 
 type UpdatePlayerRequest struct {
-	PlayerName		string	`json:"playerName"`
+	Name			string	`json:"name"`
 	Team			string  `json:"team"`
-	Position		string	`json:"position"`
 	Games			int	`json:"games"`
 	PA			int	`json:"plateAppearances"`
 	HR			int	`json:"homeRuns"`
@@ -54,13 +52,12 @@ type UpdatePlayerRequest struct {
 
 type Player struct {
 	ID			int	        `json:"id"`
-	PlayerName		string	        `json:"playerName"`
+	Name			string	        `json:"name"`
 	Team			string          `json:"team"`
-	Position		string	        `json:"position"`
 	Games			int	        `json:"games"`
 	PA			int	        `json:"plateAppearances"`
 	HR			int	        `json:"homeRuns"`
-	R			int             `json:"runs"`
+	R			int		`json:"runs"`
 	RBI			int	        `json:"runsBattedIn"`
 	SB			int	        `json:"stolenBases"`
 	BbRate			float32	        `json:"walkRate"`
@@ -78,35 +75,10 @@ type Player struct {
 	DeletedOn		time.Time	`json:"-"`
 }
 
-type Team struct {
-	ID			int	        `json:"id"`
-	Team			string          `json:"team"`
-	Games			int	        `json:"games"`
-	PA			int	        `json:"plate_appearances"`
-	HR			int	        `json:"home_runs"`
-	R			int             `json:"runs"`
-	RBI			int	        `json:"runs_batted_in"`
-	SB			int	        `json:"stolen_bases"`
-	BbRate			float32	        `json:"walk_rate(BB%)"`
-	KRate			float32	        `json:"strikeout_rate(K%)"`
-	ISO			float32	        `json:"isolated_power"`
-	BABIP			float32         `json:"batting_avg_balls_in_play"`
-	AVG			float32	        `json:"batting_avg"`
-	OBP			float32	        `json:"on_base_pct"`
-	SLG			float32	        `json:"slugging_pct"`
-	WOBA			float32	        `json:"weighted_on_base_avg"`
-	WRCPlus			int	        `json:"weighted_runs_created_plus"`	
-	TeamWAR			float32	        `json:"team_war"`
-	CreatedOn		time.Time	`json:""`
-	UpdatedOn		time.Time	`json:"-"`
-	DeletedOn		time.Time	`json:"-"`
-}
-
-func NewPlayer(playerName, team, position string, games, pa, hr, r, rbi, sb, wrcPlus int, bbRate, kRate, iso, babip, avg, obp, slg, woba, lastSeasonWAR float32) *Player {
+func NewPlayer(name, team string, games, pa, hr, r, rbi, sb, wrcPlus int, bbRate, kRate, iso, babip, avg, obp, slg, woba, lastSeasonWAR float32) *Player {
 	return &Player{
-		PlayerName: playerName,
-		Team:	team,
-		Position: position,
+		Name: name,
+		Team: team,
 		Games: games,
 		PA: pa,
 		HR: hr,
@@ -123,6 +95,5 @@ func NewPlayer(playerName, team, position string, games, pa, hr, r, rbi, sb, wrc
 		WOBA: woba,
 		WRCPlus: wrcPlus,
 		LastSeasonWAR: lastSeasonWAR,
-		CreatedOn: time.Now().UTC(),
 	}
 }
