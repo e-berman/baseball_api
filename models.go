52d75867e9414a40ff7023c81df8098b00d5ebf6
e-berman <ethanjamesberman@gmail.com>
2023-04-27 21:10:04 -0700

    remove fields from models

--- models.go
+++ models.go
@@ -1,99 +1,76 @@
 package main
 
-import "time"
-
 type CreatePlayerRequest struct {
-	Name			string	`json:"name"`
-	Team			string  `json:"team"`
-	Games			int	`json:"games"`
-	PA			int	`json:"plateAppearances"`
-	HR			int	`json:"homeRuns"`
-	R			int     `json:"runs"`
-	RBI			int	`json:"runsBattedIn"`
-	SB			int	`json:"stolenBases"`
-	BbRate			float32	`json:"walkRate"`
-	KRate			float32	`json:"strikeoutRate"`
-	ISO			float32	`json:"isolatedPower"`
-	BABIP			float32 `json:"battingAvgBallsInPlay"`
-	AVG			float32	`json:"battingAvg"`
-	OBP			float32	`json:"onBasePct"`
-	SLG			float32	`json:"sluggingPct"`
-	WOBA			float32	`json:"weightedOnBaseAvg"`
-	WRCPlus			int	`json:"weightedRunsCreatedPlus"`	
-	LastSeasonWAR		float32	`json:"lastSeasonWar"`
-	CreatedOn		string	`json:"-"`
-	UpdatedOn		string	`json:"-"`
-	DeletedOn		string	`json:"-"`
+	Name   string  `json:"name"`
+	Team   string  `json:"team"`
+	Games  int     `json:"games"`
+	PA     int     `json:"plateAppearances"`
+	HR     int     `json:"homeRuns"`
+	R      int     `json:"runs"`
+	RBI    int     `json:"runsBattedIn"`
+	SB     int     `json:"stolenBases"`
+	BbRate float64 `json:"walkRate"`
+	KRate  float64 `json:"strikeoutRate"`
+	ISO    float64 `json:"isolatedPower"`
+	AVG    float64 `json:"battingAvg"`
+	OBP    float64 `json:"onBasePct"`
+	SLG    float64 `json:"sluggingPct"`
+	WOBA   float64 `json:"weightedOnBaseAvg"`
 }
 
 type UpdatePlayerRequest struct {
-	Name			string	`json:"name"`
-	Team			string  `json:"team"`
-	Games			int	`json:"games"`
-	PA			int	`json:"plateAppearances"`
-	HR			int	`json:"homeRuns"`
-	R			int     `json:"runs"`
-	RBI			int	`json:"runsBattedIn"`
-	SB			int	`json:"stolenBases"`
-	BbRate			float32	`json:"walkRate"`
-	KRate			float32	`json:"strikeoutRate"`
-	ISO			float32	`json:"isolatedPower"`
-	BABIP			float32 `json:"battingAvgBallsInPlay"`
-	AVG			float32	`json:"battingAvg"`
-	OBP			float32	`json:"onBasePct"`
-	SLG			float32	`json:"sluggingPct"`
-	WOBA			float32	`json:"weightedOnBaseAvg"`
-	WRCPlus			int	`json:"weightedRunsCreatedPlus"`	
-	LastSeasonWAR		float32	`json:"lastSeasonWar"`
-	CreatedOn		string	`json:"-"`
-	UpdatedOn		string	`json:"-"`
-	DeletedOn		string	`json:"-"`
+	Name   string  `json:"name"`
+	Team   string  `json:"team"`
+	Games  int     `json:"games"`
+	PA     int     `json:"plateAppearances"`
+	HR     int     `json:"homeRuns"`
+	R      int     `json:"runs"`
+	RBI    int     `json:"runsBattedIn"`
+	SB     int     `json:"stolenBases"`
+	BbRate float64 `json:"walkRate"`
+	KRate  float64 `json:"strikeoutRate"`
+	ISO    float64 `json:"isolatedPower"`
+	AVG    float64 `json:"battingAvg"`
+	OBP    float64 `json:"onBasePct"`
+	SLG    float64 `json:"sluggingPct"`
+	WOBA   float64 `json:"weightedOnBaseAvg"`
 }
 
 type Player struct {
-	ID			int	        `json:"id"`
-	Name			string	        `json:"name"`
-	Team			string          `json:"team"`
-	Games			int	        `json:"games"`
-	PA			int	        `json:"plateAppearances"`
-	HR			int	        `json:"homeRuns"`
-	R			int		`json:"runs"`
-	RBI			int	        `json:"runsBattedIn"`
-	SB			int	        `json:"stolenBases"`
-	BbRate			float32	        `json:"walkRate"`
-	KRate			float32	        `json:"strikeoutRate"`
-	ISO			float32	        `json:"isolatedPower"`
-	BABIP			float32         `json:"battingAvgBallsInPlay"`
-	AVG			float32	        `json:"battingAvg"`
-	OBP			float32	        `json:"onBasePct"`
-	SLG			float32	        `json:"sluggingPct"`
-	WOBA			float32	        `json:"weightedOnBaseAvg"`
-	WRCPlus			int	        `json:"weightedRunsCreatedPlus"`	
-	LastSeasonWAR		float32	        `json:"lastSeasonWar"`
-	CreatedOn		time.Time	`json:"created_on"`
-	UpdatedOn		time.Time	`json:"-"`
-	DeletedOn		time.Time	`json:"-"`
+	ID     int     `json:"id"`
+	Name   string  `json:"name"`
+	Team   string  `json:"team"`
+	Games  int     `json:"games"`
+	PA     int     `json:"plateAppearances"`
+	HR     int     `json:"homeRuns"`
+	R      int     `json:"runs"`
+	RBI    int     `json:"runsBattedIn"`
+	SB     int     `json:"stolenBases"`
+	BbRate float64 `json:"walkRate"`
+	KRate  float64 `json:"strikeoutRate"`
+	ISO    float64 `json:"isolatedPower"`
+	AVG    float64 `json:"battingAvg"`
+	OBP    float64 `json:"onBasePct"`
+	SLG    float64 `json:"sluggingPct"`
+	WOBA   float64 `json:"weightedOnBaseAvg"`
 }
 
-func NewPlayer(name, team string, games, pa, hr, r, rbi, sb, wrcPlus int, bbRate, kRate, iso, babip, avg, obp, slg, woba, lastSeasonWAR float32) *Player {
+func NewPlayer(name, team string, games, pa, hr, r, rbi, sb int, bbRate, kRate, iso, avg, obp, slg, woba float64) *Player {
 	return &Player{
-		Name: name,
-		Team: team,
-		Games: games,
-		PA: pa,
-		HR: hr,
-		R: r,
-		RBI: rbi,
-		SB: sb,
+		Name:   name,
+		Team:   team,
+		Games:  games,
+		PA:     pa,
+		HR:     hr,
+		R:      r,
+		RBI:    rbi,
+		SB:     sb,
 		BbRate: bbRate,
-		KRate: kRate,
-		ISO: iso,
-		BABIP: babip,
-		AVG: avg,
-		OBP: obp,
-		SLG: slg,
-		WOBA: woba,
-		WRCPlus: wrcPlus,
-		LastSeasonWAR: lastSeasonWAR,
+		KRate:  kRate,
+		ISO:    iso,
+		AVG:    avg,
+		OBP:    obp,
+		SLG:    slg,
+		WOBA:   woba,
 	}
 }
