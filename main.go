3c2589c0d241c080235035d5cc1892ab8a6c55b5
e-berman <ethanjamesberman@gmail.com>
2023-04-29 21:48:31 -0700

    add option for adding filepath from stdin

--- main.go
+++ main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"strings"
 )
 
+var Fp string
+
 func main() {
+	var choice string
+
+	fmt.Println("Do you want to import a csv file?: ")
+	fmt.Println("[Y]: Yes")
+	fmt.Println("[N]: No")
+	fmt.Scanln(&choice)
+	fmt.Println("You entered: ", choice)
+
+	if strings.ToLower(choice) == "y" {
+		fmt.Println("Enter the absolute filepath of the csv import file: ")
+		fmt.Scanln(&Fp)
+	}
+
 	dbpool, err := NewDBPool()
 	if err != nil {
 		log.Fatal(err)
