cc44b71df750f011d0aed8faa4bc9fdf4a7234a2
e-berman <ethanjamesberman@gmail.com>
2023-05-04 22:16:41 -0700

    add swagger config

---
+++ swagger.go
@@ -0,0 +1,17 @@
+// Package classification Baseball API
+//
+// This application is a REST API for baseball players stats
+//
+//	Schemes: http
+//	Host: localhost
+//	BasePath: /
+//	Version: 0.0.1
+//
+//	Consumes:
+//	- application/json
+//
+//	Produces:
+//	- application/json
+//
+// swagger:meta
+package main
