b1a82a83e68c439f6a104cdc9cd2dd28c43669f1
e-berman <ethanjamesberman@gmail.com>
2023-05-24 15:04:02 -0700

    change csv import on initialization

--- main.go
+++ main.go
@@ -11,16 +11,6 @@ var Fp string
 func main() {
 	var choice string
 
-	fmt.Println("Do you want to import a csv file?: ")
-	fmt.Println("[Y]: Yes")
-	fmt.Println("[N]: No")
-	fmt.Scanln(&choice)
-
-	if strings.ToLower(choice) == "y" {
-		fmt.Println("Enter the absolute filepath of the csv import file: ")
-		fmt.Scanln(&Fp)
-	}
-
 	dbpool, err := NewDBPool()
 	if err != nil {
 		log.Fatal(err)
@@ -29,6 +19,20 @@ func main() {
 
 	if err := dbpool.InitializePlayerTable(); err != nil {
 		log.Fatal(err)
+	}
+
+	fmt.Println("Do you want to import a csv file?: ")
+	fmt.Println("[Y]: Yes")
+	fmt.Println("[N]: No")
+	fmt.Scanln(&choice)
+
+	if strings.ToLower(choice) == "y" {
+		fmt.Println("Enter the absolute filepath of the csv import file: ")
+		fmt.Scanln(&Fp)
+
+		if err := dbpool.ImportDataFromCSV(); err != nil {
+			log.Fatal(err)
+		}
 	}
 
 	server := NewServer(":4242", dbpool)
