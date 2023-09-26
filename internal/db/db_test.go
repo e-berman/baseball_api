cbb02b19ae51c4faa17b71e22950d7ff9edf2761
e-berman <ethanjamesberman@gmail.com>
2023-09-25 22:04:06 -0700

    add db testing skeleton

---
+++ internal/db/db_test.go
@@ -0,0 +1,31 @@
+package db
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConvertToInt(t *testing.T) {
+}
+
+func TestConvertToFloat(t *testing.T) {
+
+}
+
+func TestFloatToInt(t *testing.T) {
+	
+}
+
+func TestRoundFloat(t *testing.T) {
+
+}
+
+func TestReadFromCSVPositionPlayer(t *testing.T) {
+
+}
+
+func TestReadFromCSVPitcher(t *testing.T) {
+
+}
